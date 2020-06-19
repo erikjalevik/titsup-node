eb03bc0a172ed3999cc29633da39264c71a19895
Erik Jälevik <erikjalevik@fastmail.fm>
2020-06-19 14:12:35 +0200

    Get rid of mounting and just use rsync's built-in ssh capability
    Much faster!

--- titsup.sh
+++ titsup.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-umount /Volumes/share
-mkdir /tmp/share
-mount -t afp afp://admin:p0wersh0werNAS@192.168.0.14/share /tmp/share
-~/.nvm/versions/node/v12.5.0/bin/node ~/dev/titsup-node/titsup.js ~ /tmp/share/Backup/Mac/ejal
-umount /tmp/share
-rmdir /tmp/share
+~/.nvm/versions/node/v12.5.0/bin/node ~/dev/titsup-node/titsup.js ~ admin@192.168.0.14:/volume1/share/Backup/Mac/ejal
