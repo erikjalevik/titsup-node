9ee8ee7a9fc675a508aa3f379db661e2f74d56df
Erik Jälevik <erikjalevik@fastmail.fm>
2020-09-25 11:11:08 +0200

    Add comment

--- titsup.sh
+++ titsup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+#
+# The script to run on Mac to back up the home directory.
 
 ~/.nvm/versions/node/v12.5.0/bin/node ~/dev/titsup-node/titsup.js ~ admin@192.168.0.14:/volume1/share/Backup/Mac/ejal
